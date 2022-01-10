03817f0aeb3f480a8c897fa09e8d5ac9f6dd9f3c
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-01-10 15:06:28 +0200

    kurssitiedot step7 - tehtävien lkm reduce-funktiolla

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const Total = ({course}) => {
+  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
+  return (
+    <div>
+      <p><b>total of exercises {total}</b></p>
+    </div>
+  )
+}
+
 const Part = (props) => {
   return (
     <div>
@@ -62,6 +71,7 @@ const App = () => {
   return (
     <div>
       <Course course={course} />
+      <Total course={course} />
     </div>
   )
 }
