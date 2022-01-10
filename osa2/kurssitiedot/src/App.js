e72a86e7549b7fa5fb5d6da01ea43d8b3a365ee8
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-01-10 14:50:46 +0200

    kurssitiedot step6 - Course komponentti ja  osat map-funktiolla

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course = {course} />
+      <Content course = {course}  />
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
