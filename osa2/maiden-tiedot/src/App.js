d4636689cf187776792a041a5111dea7639c9344
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-01-13 13:57:03 +0200

    step2 - listassa näytettäviin maihin nappi, jolla yksittäisen maan tiedot näkyville

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+// Komponentti hakukentälle
 const Search = ({setSearch}) => {
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
@@ -10,6 +11,7 @@ const Search = ({setSearch}) => {
   ) 
 }
 
+// Komponentti maassa puhuttujen kielien näyttämiseen
 const Languages = ({country}) => {
   return (
     <div>
@@ -22,13 +24,51 @@ const Languages = ({country}) => {
   )
 }
 
+// Komponentti maan lipun näyttämistä varten
 const Flag = ({country}) => {
   const src = country.flags.png
   console.log(src)
   return <div><img src={src}/></div>
 }
 
-const Results = ({countries, search}) => {
+// Painike -komponentti
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+// Komponentti joka näyttää listan annetuista maista
+const CountryList = ({countriesToShow, handleShow}) => (
+  <div>
+    {countriesToShow.map(country =>
+      <p key={country.name.common}>{country.name.common} <Button handleClick={() => handleShow(country.name.common)} text="show"></Button></p>
+    )}
+  </div>
+)
+
+// Komponentti yksittäisen maan tietojen näyttämiseen
+const SingleCountry = ({countriesToShow}) => (
+  <div>
+    {countriesToShow.map(country => 
+      <div key={country.name.common}>
+        <h3>{country.name.common}</h3>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h4>Languages</h4>
+        <Languages country={country}/>
+        <Flag country = {country} />
+      </div>
+    )}
+  </div>
+)
+
+/* Komponentti hakutulosten näyttämiseen
+ * Jos löydetään yli 10 maata, näytetään varoitusteksti "too many countries..."
+ * Jos löydetään 2-10 maata, näytetään lista
+ * Jos löydetään 1 maa, näytetään tarkemmat tiedot
+ */
+const Results = ({countries, search, handleShow}) => {
   const countriesToShow = countries.filter(country => country.name.common.toString().toLowerCase().includes(search.toLowerCase()))
   if (countriesToShow.length > 10) {
       return (
@@ -37,37 +77,20 @@ const Results = ({countries, search}) => {
         </div>
       )
   }
-  else if (countriesToShow.length > 1) {
-    return (
-      <div>
-        {countriesToShow.map(country =>
-          <p key={country.name.common}>{country.name.common}</p>
-        )}
-      </div>
-    )
-  }
-  else {
-    return (
-      <div>
-        {countriesToShow.map(country => 
-          <div key={country.name.common}>
-            <h3>{country.name.common}</h3>
-            <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
-            <h4>Languages</h4>
-            <Languages country={country}/>
-            <Flag country = {country} />
-          </div>
-        )}
-      </div>
-    )  
-  }
+  else if (countriesToShow.length > 1) 
+    return <CountryList countriesToShow={countriesToShow} handleShow ={handleShow} />
+  else 
+    return <SingleCountry countriesToShow={countriesToShow} />
 }
 
 function App() {
 // Tilat
 const [countries, setCountries] = useState([])
 const [search, setSearch] = useState('')
+
+// Käsittelee "show"-napin painamisen
+const handleShow = (name) => setSearch(name) 
+  
 
 // effect-hook maiden hakemiseen palvelimelta
 useEffect(() => {
@@ -83,7 +106,7 @@ useEffect(() => {
       <h1>Country finder</h1>
       <Search setSearch = {setSearch} />
       <h2>Results</h2>
-      <Results countries = {countries} search={search}/>
+      <Results countries = {countries} search={search} handleShow={handleShow}/>
     </div>
   );
 }
