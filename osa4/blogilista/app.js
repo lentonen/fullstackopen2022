4abc499504e260a2dcbdda145c181cef52948968
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-16 15:08:13 +0200

    blogilista step2 - ohjelma jaettu osiin

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+      logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+      logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
