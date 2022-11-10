329013c68f4409ffd71eaf5d6eb2c1641327fb20
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-11-10 19:55:55 +0200

    blogilistan laajennus step6 - token perusteinen autentikointi

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
