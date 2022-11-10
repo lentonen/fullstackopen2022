0403d49b892ccd3d8d861f26c26bcf4e53035cdc
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-11-10 20:49:20 +0200

    blogilistan laajennus step7 - blogin lisääminen pyynnössä mukana olevan tokenin avulla

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,7 +1,9 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const { request, response } = require('../app')
 const Blog =require('../models/blog')
 const User = require('../models/users')
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -9,10 +11,21 @@ blogsRouter.get('/', async (request, response) => {
 })
   
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  let decodedToken
+  try {
+    decodedToken = jwt.verify(token, process.env.secret)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+  } catch (error) {
+    next(error)
+  }
+ 
 
-  const user = await User.findOne()
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     id: body.id,
@@ -25,7 +38,6 @@ blogsRouter.post('/', async (request, response) => {
 
   if (typeof blog.title !== 'undefined' && typeof blog.url !== 'undefined') {
     const savedBlog = await blog.save()
-    console.log(savedBlog._id)
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     response.status(201).json(savedBlog)
@@ -34,6 +46,15 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   } 
 })
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 
 blogsRouter.delete('/:id', async (request, response) => {
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -2,6 +2,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json( {error: 'invalid token' })
   }
   next(error)
 }
